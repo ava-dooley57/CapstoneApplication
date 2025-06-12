f6d2780603e8299005b9c58a5fd160244bf7d643
Ava <ava.dooley57@ed.cna.nl.ca>
2025-06-12 01:33:01 -0230

    Initial Commit

---
+++ CapstoneApplication/CapstoneApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApplicationApp.swift
+//  CapstoneApplication
+//
+//  Created by Ava Dooley on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapstoneApplicationTests/CapstoneApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApplicationTests.swift
+//  CapstoneApplicationTests
+//
+//  Created by Ava Dooley on 2025-06-12.
+//
+
+import Testing
+@testable import CapstoneApplication
+
+struct CapstoneApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
